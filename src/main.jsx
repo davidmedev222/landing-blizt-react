f44af3cd44b5dee1ae56e4e3083f1afa611465b9
davidmedev222 <davidmedev@gmail.com>
2023-02-03 16:15:13 -0300

    feat(web): add theme with context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './context/theme/ThemeProvider';
 import { App } from './routes/App';
 import './styles/App.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
