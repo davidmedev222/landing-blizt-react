0a8eddc31fecc12fb00a1b8e6f610ac351fb7eb7
davidmedev222 <davidmedev@gmail.com>
2023-02-03 13:24:08 -0300

    feat(web): add pre-loader for the web

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import { Triangle } from 'react-loader-spinner';
+
+const Loader = () => {
+  const [loaderClass, updateLoaderClass] = useState('loader');
+
+  useEffect(() => {
+    setTimeout(() => {
+      updateLoaderClass('loader loader-active');
+    }, 3000);
+  }, []);
+
+  return <Triangle wrapperClass={loaderClass} color="#ffffff" ariaLabel="triangle-loading" />;
+};
+
+export { Loader };
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'; // COMPONENTS
 import { Home } from '../pages/home/Home'; // COMPONENT
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export { App };
