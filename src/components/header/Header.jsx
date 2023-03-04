08e6835a8351bb17a334d3082fadb8f054e5f41d
davidmedev222 <davidmedev@gmail.com>
2023-03-04 19:17:11 -0300

    chore(web): add link to home in the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import { ThemeContext } from '../../context/theme/ThemeContext'
 import { Navbar, Button } from '../export'
 import { Theme } from '../theme/Theme'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
@@ -9,7 +10,7 @@ const Header = () => {
   return (
     <header data-fade className='header'>
       {/* TITLE */}
-      <h2 className='header-title'>blizt</h2>
+      <Link to='/' className='header-a'>blizt</Link>
       {/* NAVBAR */}
       <Navbar />
       {/* BUTTON */}
