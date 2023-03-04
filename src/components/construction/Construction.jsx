2a34014381dae45f546eb427be1bb1da45220be1
davidmedev222 <davidmedev@gmail.com>
2023-03-04 19:16:36 -0300

    chore(web): add interface of the page construction

---
+++ src/components/construction/Construction.jsx
@@ -0,0 +1,12 @@
+import { IconConstructor } from '../export'
+
+const Construction = () => {
+  return (
+    <section className='construction'>
+      <IconConstructor className='construction-svg' />
+      <h2 className='construction-heading'>Coming soon...</h2>
+    </section>
+  )
+}
+
+export { Construction }
