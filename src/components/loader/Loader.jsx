98eca450eb082926f35b7876892df3637a4402b6
davidmedev222 <davidmedev@gmail.com>
2023-03-05 02:38:53 -0300

    refactor(web): remove code not utilized

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,16 +1,7 @@
-import { useEffect, useState } from 'react'
 import { Triangle } from 'react-loader-spinner'
 
 const Loader = () => {
-  const [show, updateShow] = useState(true)
-
-  useEffect(() => {
-    const show = setTimeout(() => updateShow(false), 5000)
-
-    return () => clearTimeout(show)
-  }, [])
-
-  return show && <Triangle wrapperClass='loader' color='#ffffff' ariaLabel='triangle-loading' />
+  return <Triangle wrapperClass='loader' color='#ffffff' ariaLabel='triangle-loading' />
 }
 
 export { Loader }
