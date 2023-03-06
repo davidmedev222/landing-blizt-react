d986a6c347606ec644ec7558025d86be18cdd16e
davidmedev222 <davidmedev@gmail.com>
2023-03-06 17:07:32 -0300

    perf(web): add loader for the rendering to the web

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from 'react'
 import { Triangle } from 'react-loader-spinner'
 
 const Loader = () => {
-  return <Triangle wrapperClass='loader' color='#ffffff' ariaLabel='triangle-loading' />
+  const [isLoading, updateIsLoading] = useState(true)
+
+  useEffect(() => {
+    const show = setTimeout(() => updateIsLoading(false), 4000)
+
+    return () => clearTimeout(show)
+  }, [])
+
+  return isLoading && <Triangle wrapperClass='loader' color='#ffffff' ariaLabel='triangle-loading' />
 }
 
 export { Loader }
--- src/pages/layout/LayoutPages.jsx
+++ src/pages/layout/LayoutPages.jsx
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react'
-
 import { Outlet } from 'react-router-dom'
 import { Header, Loader } from '../../components/export'
 
 const LayoutPages = () => {
-  const [isLoading, updateIsLoading] = useState(true)
-
-  useEffect(() => {
-    const show = setTimeout(() => updateIsLoading(false), 4000)
-
-    return () => clearTimeout(show)
-  }, [])
-
-  return isLoading ? <Loader /> : <> <Header /> <Outlet /> </>
+  return (
+    <>
+      <Loader />
+      <Header />
+      <Outlet />
+    </>
+  )
 }
 
 export { LayoutPages }
